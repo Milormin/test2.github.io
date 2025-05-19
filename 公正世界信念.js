ab22eb2d3f1925be202156a40d6199b85e0e403f
Milormin <1124489495@qq.com>
2025-05-19 14:29:42 +0800

    Add files via upload

---
+++ 公正世界信念.js
@@ -0,0 +1,583 @@
+
+var block1_stims = [
+    {
+      "stims": "我",
+      "key_association": "left"
+    },
+    {
+      "stims": "自己",
+      "key_association": "left"
+    },
+    {
+      "stims": "自己的",
+      "key_association": "left"
+    },
+    {
+      "stims": "咱们",
+      "key_association": "left"
+    },
+    {
+      "stims": "本人",
+      "key_association": "left"
+      },
+    {
+      "stims": "俺",
+      "key_association": "left"
+      },
+    {
+      "stims": "本人的",
+      "key_association": "left"
+      },
+    {
+      "stims": "我的",
+      "key_association": "left"
+      },
+    {
+      "stims": "我们的",
+      "key_association": "left"
+      },
+    {
+      "stims": "俺们",
+      "key_association": "left"
+      },
+    {
+      "stims": "他们的",
+      "key_association": "right"
+    },
+    {
+      "stims": "他人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "别人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "外人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "他们",
+      "key_association": "right"
+    },
+    {
+      "stims": "她们的",
+      "key_association": "right"
+    },
+    {
+      "stims": "她",
+      "key_association": "right"
+    },
+    {
+      "stims": "她们",
+      "key_association": "right"
+    },
+    {
+      "stims": "其他",
+      "key_association": "right"
+    },
+    {
+      "stims": "其他的",
+      "key_association": "right"
+    }
+  ]
+  
+  
+  var block2_stims = [
+    {
+      "stims": "公道",
+      "key_association": "left"
+    },
+    {
+      "stims": "公允",
+      "key_association": "left"
+    },
+    {
+      "stims": "公平",
+      "key_association": "left"
+    },
+    {
+      "stims": "刚正",
+      "key_association": "left"
+    },
+    {
+      "stims": "民主",
+      "key_association": "left"
+      },
+    {
+      "stims": "公正",
+      "key_association": "left"
+      },
+    {
+      "stims": "正当",
+      "key_association": "left"
+      },
+    {
+      "stims": "平等",
+      "key_association": "left"
+      },
+    {
+      "stims": "客观",
+      "key_association": "left"
+      },
+    {
+      "stims": "廉洁",
+      "key_association": "left"
+      },
+    {
+      "stims": "冤枉",
+      "key_association": "right"
+    },
+    {
+      "stims": "陷害",
+      "key_association": "right"
+    },
+    {
+      "stims": "贪污",
+      "key_association": "right"
+    },
+    {
+      "stims": "独裁",
+      "key_association": "right"
+    },
+    {
+      "stims": "偏袒",
+      "key_association": "right"
+    },
+    {
+      "stims": "包庇",
+      "key_association": "right"
+    },
+    {
+      "stims": "袒护",
+      "key_association": "right"
+    },
+    {
+      "stims": "不公",
+      "key_association": "right"
+    },
+    {
+      "stims": "偏见",
+      "key_association": "right"
+    },
+    {
+      "stims": "侵占",
+      "key_association": "right"
+    }
+  ]
+  
+  
+  
+  
+  
+  var block34_stims = [
+    {
+      "stims": "我",
+      "key_association": "left"
+    },
+    {
+      "stims": "自己",
+      "key_association": "left"
+    },
+    {
+      "stims": "自己的",
+      "key_association": "left"
+    },
+    {
+      "stims": "咱们",
+      "key_association": "left"
+    },
+    {
+      "stims": "本人",
+      "key_association": "left"
+      },
+    {
+      "stims": "俺",
+      "key_association": "left"
+      },
+    {
+      "stims": "本人的",
+      "key_association": "left"
+      },
+    {
+      "stims": "我的",
+      "key_association": "left"
+      },
+    {
+      "stims": "我们的",
+      "key_association": "left"
+      },
+    {
+      "stims": "俺们",
+      "key_association": "left"
+      },
+    {
+      "stims": "他们的",
+      "key_association": "right"
+    },
+    {
+      "stims": "他人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "别人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "外人的",
+      "key_association": "right"
+    },
+    {
+      "stims": "他们",
+      "key_association": "right"
+    },
+    {
+      "stims": "她们的",
+      "key_association": "right"
+    },
+    {
+      "stims": "她",
+      "key_association": "right"
+    },
+    {
+      "stims": "她们",
+      "key_association": "right"
+    },
+    {
+      "stims": "其他",
+      "key_association": "right"
+    },
+    {
+      "stims": "其他的",
+      "key_association": "right"
+    },
+    {
+      "stims": "公道",
+      "key_association": "left"
+    },
+    {
+      "stims": "公允",
+      "key_association": "left"
+    },
+    {
+      "stims": "公平",
+      "key_association": "left"
+    },
+    {
+      "stims": "刚正",
+      "key_association": "left"
+    },
+    {
+      "stims": "民主",
+      "key_association": "left"
+      },
+    {
+      "stims": "公正",
+      "key_association": "left"
+      },
+    {
+      "stims": "正当",
+      "key_association": "left"
+      },
+    {
+      "stims": "平等",
+      "key_association": "left"
+      },
+    {
+      "stims": "客观",
+      "key_association": "left"
+      },
+    {
+      "stims": "廉洁",
+      "key_association": "left"
+      },
+    {
+      "stims": "冤枉",
+      "key_association": "right"
+    },
+    {
+      "stims": "陷害",
+      "key_association": "right"
+    },
+    {
+      "stims": "贪污",
+      "key_association": "right"
+    },
+    {
+      "stims": "独裁",
+      "key_association": "right"
+    },
+    {
+      "stims": "偏袒",
+      "key_association": "right"
+    },
+    {
+      "stims": "包庇",
+      "key_association": "right"
+    },
+    {
+      "stims": "袒护",
+      "key_association": "right"
+    },
+    {
+      "stims": "不公",
+      "key_association": "right"
+    },
+    {
+      "stims": "偏见",
+      "key_association": "right"
+    },
+    {
+      "stims": "侵占",
+      "key_association": "right"
+    }
+  ]
+  
+  var block5_stims = [
+   {
+      "stims": "我",
+      "key_association": "right"
+    },
+    {
+      "stims": "自己",
+      "key_association": "right"
+    },
+    {
+      "stims": "自己的",
+      "key_association": "right"
+    },
+    {
+      "stims": "咱们",
+      "key_association": "right"
+    },
+    {
+      "stims": "本人",
+      "key_association": "right"
+      },
+    {
+      "stims": "俺",
+      "key_association": "right"
+      },
+    {
+      "stims": "本人的",
+      "key_association": "right"
+      },
+    {
+      "stims": "我的",
+      "key_association": "right"
+      },
+    {
+      "stims": "我们的",
+      "key_association": "right"
+      },
+    {
+      "stims": "俺们",
+      "key_association": "right"
+      },
+    {
+      "stims": "他们的",
+      "key_association": "left"
+    },
+    {
+      "stims": "他人的",
+      "key_association": "left"
+    },
+    {
+      "stims": "别人的",
+      "key_association": "left"
+    },
+    {
+      "stims": "外人的",
+      "key_association": "left"
+    },
+    {
+      "stims": "他们",
+      "key_association": "left"
+    },
+    {
+      "stims": "她们的",
+      "key_association": "left"
+    },
+    {
+      "stims": "她",
+      "key_association": "left"
+    },
+    {
+      "stims": "她们",
+      "key_association": "left"
+    },
+    {
+      "stims": "其他",
+      "key_association": "left"
+    },
+    {
+      "stims": "其他的",
+      "key_association": "left"
+    }
+  ]
+  
+  
+  var block67_stims = [
+    {
+        "stims": "我",
+        "key_association": "right"
+      },
+      {
+        "stims": "自己",
+        "key_association": "right"
+      },
+      {
+        "stims": "自己的",
+        "key_association": "right"
+      },
+      {
+        "stims": "咱们",
+        "key_association": "right"
+      },
+      {
+        "stims": "本人",
+        "key_association": "right"
+        },
+      {
+        "stims": "俺",
+        "key_association": "right"
+        },
+      {
+        "stims": "本人的",
+        "key_association": "right"
+        },
+      {
+        "stims": "我的",
+        "key_association": "right"
+        },
+      {
+        "stims": "我们的",
+        "key_association": "right"
+        },
+      {
+        "stims": "俺们",
+        "key_association": "right"
+        },
+      {
+        "stims": "他们的",
+        "key_association": "left"
+      },
+      {
+        "stims": "他人的",
+        "key_association": "left"
+      },
+      {
+        "stims": "别人的",
+        "key_association": "left"
+      },
+      {
+        "stims": "外人的",
+        "key_association": "left"
+      },
+      {
+        "stims": "他们",
+        "key_association": "left"
+      },
+      {
+        "stims": "她们的",
+        "key_association": "left"
+      },
+      {
+        "stims": "她",
+        "key_association": "left"
+      },
+      {
+        "stims": "她们",
+        "key_association": "left"
+      },
+      {
+        "stims": "其他",
+        "key_association": "left"
+      },
+      {
+        "stims": "其他的",
+        "key_association": "left"
+      },
+      {
+        "stims": "公道",
+        "key_association": "left"
+      },
+      {
+        "stims": "公允",
+        "key_association": "left"
+      },
+      {
+        "stims": "公平",
+        "key_association": "left"
+      },
+      {
+        "stims": "刚正",
+        "key_association": "left"
+      },
+      {
+        "stims": "民主",
+        "key_association": "left"
+        },
+      {
+        "stims": "公正",
+        "key_association": "left"
+        },
+      {
+        "stims": "正当",
+        "key_association": "left"
+        },
+      {
+        "stims": "平等",
+        "key_association": "left"
+        },
+      {
+        "stims": "客观",
+        "key_association": "left"
+        },
+      {
+        "stims": "廉洁",
+        "key_association": "left"
+        },
+      {
+        "stims": "冤枉",
+        "key_association": "right"
+      },
+      {
+        "stims": "陷害",
+        "key_association": "right"
+      },
+      {
+        "stims": "贪污",
+        "key_association": "right"
+      },
+      {
+        "stims": "独裁",
+        "key_association": "right"
+      },
+      {
+        "stims": "偏袒",
+        "key_association": "right"
+      },
+      {
+        "stims": "包庇",
+        "key_association": "right"
+      },
+      {
+        "stims": "袒护",
+        "key_association": "right"
+      },
+      {
+        "stims": "不公",
+        "key_association": "right"
+      },
+      {
+        "stims": "偏见",
+        "key_association": "right"
+      },
+      {
+        "stims": "侵占",
+        "key_association": "right"
+      }
+  ]
+  
+  
